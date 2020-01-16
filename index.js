cd75ec15d8a6802e38a3ec20a3f605d0b177090f
John Afolabi <johnafolabi.da@gmail.com>
2020-01-16 15:08:36 +0100

    Create a simple express server

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const server = require("./server/server");
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`\n Server started on port ${port} \n`);
+});
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get("/api", (req, res) => {
+  res.status(200).json("This API works correctly");
+});
+
+module.exports = server;
